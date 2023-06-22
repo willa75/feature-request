c52fbfd31cc05f6b7b9990d7a473188d340646dc
anthony.williams <anthonywi99@yahoo.com>
2023-06-22 08:20:40 -0400

    convert AC/gerkin comments to code structure

--- __tests__/test_cases/e2e/request.test.js
+++ __tests__/test_cases/e2e/request.test.js
@@ -1,19 +1,85 @@
-/**
- * given an authenticated user
- * when they submit a feauture request
- * it should be visible when getting an updated list of feature requests
- * 
- * given another authenticated user
- * when they accidentally downVote user A's request
- * the count should be updated in the list
- * 
- * when user B realizes they accidentally downVoted and triggers an upVote
- * then the count should be updated
- * 
- * given a third user,
- * when they upVote User A's request
- * the count should be updated
- * 
- * When user C removes their vote
- * the count should be updated
- */
+require('../../steps/init');
+const given = require('../../steps/given');
+const when = require('../../steps/when');
+const then = require('../../steps/then');
+const chance = require('chance').Chance();
+
+describe('Given an authenticated user', () => {
+  let user;
+  beforeAll( async () => {
+    user = await given.an_authenticated_user();
+  });
+
+  describe('When they submit a feature request', () => {
+    let request;
+    const text = chance.string({ length: 250 });
+    beforeAll( async () => {
+      request = await when.a_user_calls_addRequest(user, text);
+    });
+
+    it('should return the new request', () => {
+      expect(request).toMatchObject({
+        text: request.text
+      });
+    });
+
+    describe('When they call getRequests', () => {
+      let requests, nextToken;
+      beforeAll(async () => {
+        const result = await when.a_user_calls_getRequests(user.username, 25);
+        requests = result.requests;
+        nextToken = result.nextToken;
+      });
+
+      it('They will see the new request in the requests array', async () => {
+        expect(nextToken).toBeNull();
+        expect(requests.length).toEqual(1);
+        expect(requests[0]).toEqual(request);
+      });
+    });
+
+    describe('When they like the request', () => {
+      beforeAll(async () => {
+        await when.a_user_likes_a_request(user, request.id);
+      });
+
+      it('Should see Request.reviewed as true', async () => {
+        const { requests } = await when.a_user_calls_getRequests(user.username, 25);
+
+        expect(requests).toHaveLength(1);
+        expect(requests[0].id).toEqual(request.id);
+        expect(requests[0].reviewed).toEqual(true);
+      });
+
+      it('Should not be able to like the same request a second time', async () => {
+        await expect(when.a_user_likes_a_request(user, request.id))
+        .rejects
+        .toMatchObject({
+          message: expect.stringContaining('DynamoDB transaction error')
+        });
+      });
+
+      describe('When they dislike the same request', () => {
+        beforeAll(async () => {
+          await when.a_user_dislikes_a_request(user, request.id);
+        });
+
+        it('Should see Request.reviewed as true', async () => {
+          const { requests } = await when.a_user_calls_getRequests(user.username, 25);
+  
+          expect(requests).toHaveLength(1);
+          expect(requests[0].id).toEqual(request.id);
+          expect(requests[0].reviewed).toEqual(true);
+        });
+
+        it('Should not be able to dislike the same request a second time', async () => {
+          await expect(when.a_user_dislikes_a_request(user, request.id))
+          .rejects
+          .toMatchObject({
+            message: expect.stringContaining('DynamoDB transaction error')
+          });
+        });
+      });
+    });
+  });
+});
--- __tests__/test_cases/int/add-request.test.js
+++ __tests__/test_cases/int/add-request.test.js
@@ -1,5 +1,29 @@
-/**
- * given an authenticated user
- * when they submit a feauture request
- * it saves it to the requests table and updates the users' request created count
- */
+require('../../steps/init');
+const given = require('../../steps/given');
+const when = require('../../steps/when');
+const then = require('../../steps/then');
+const chance = require('chance').Chance();
+
+
+describe('Given an authenticated user', () => {
+  let user;
+  beforeAll(async () => {
+    user = await given.an_authenticated_user();
+  });
+
+  describe('When the user sends a feature request', () => {
+    let request;
+    const text = chance.string({ length: 250 });
+    beforeAll(async () => {
+      request = await when.we_invoke_add_request(user.username, text);
+    });
+
+    it('Saves the request to the Requests table', async () => {
+      await then.request_exists_in_RequestsTable(request.id);
+    });
+
+    it('Increments the requestCount in the Users table to 1', async () => {
+      await then.requestCount_is_updated_in_UsersTable(user.username, 1);
+    });
+  });
+});
